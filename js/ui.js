92561b530ede93a50a6de478c6303af69e2acb76
Magdalena <m.araszczuk@o2.pl>
2025-08-22 14:08:24 +0200

    Add files via upload

--- js/ui.js
+++ js/ui.js
@@ -1,8 +1,10 @@
 // js/ui.js
 document.getElementById("year").textContent = new Date().getFullYear();
 
+/*
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js');
   });
 }
+*/
